798539a4e067d9b7e40ff873bfc20aee8fb2210e
PatsyDC <patsy.delgado@tecsup.edu.pe>
2024-07-11 16:09:58 -0500

    x

--- src/app/pages/user/seguimiento-pesca/seguimiento-pesca.component.ts
+++ src/app/pages/user/seguimiento-pesca/seguimiento-pesca.component.ts
@@ -31,7 +31,7 @@ import { MatButtonModule } from '@angular/material/button';
 export class SeguimientoPescaComponent {
   
   readonly dialog = inject(MatDialog);
-  displayedColumns: string[] = [ 'embarcacion', 'fecha', 'numero_alcance','flota', 'zona_pesca', 'estrato', 'rango_profundidad_inicial', 'rango_profundidad_final', 'tiempo_efectivo', 'rango_talla_inicial', 'rango_talla_final','moda', 'porcentaje', 'ar', 'numero', 'acciones'];
+  displayedColumns: string[] = [ 'embarcacion', 'fecha', 'numero_alcance','especie', 'zona_pesca', 'estrato', 'profundidad', 'tiempo_efectivo', 'rango_talla_inicial', 'rango_talla_final','moda', 'porcentaje', 'ar', 'numero', 'acciones'];
   dataSource: MatTableDataSource<IDiarioPesca>;
   diario: IDiarioPesca[] = []
 
